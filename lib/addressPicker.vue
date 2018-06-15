375a3e86613588633746cbfcd0ac078239c106e4
wangheyun <wangheyun@wesai.com>
2018-06-15 17:09:44 +0800

    fix area can't auto scroll

--- lib/addressPicker.vue
+++ lib/addressPicker.vue
@@ -16,119 +16,127 @@
     </mt-popup>
 </template>
 <script>
-  import {Picker} from 'mint-ui';
-  import {Popup} from 'mint-ui';
-  import info from './address';
-  let components = {};
-  components[Picker.name] = Picker;
-  components[Popup.name] = Popup;
-  export default {
-    name : 'address-picker',
-    components : components,
-    data () {
-      return {
-        slots : [
-          {
-            flex: 1,
-            values: info,
-            className: 'province',
-            textAlign: 'center'
-          },
-          {
-            flex: 1,
-            values: info[0].city,
-            className: 'city',
-            textAlign: 'center'
-          },
-          {
-            flex: 1,
-            values: info[0].city[0].district,
-            className: 'area',
-            textAlign: 'center'
-          },
-        ],
-        selectedAddress : {},
-        visible: false,
-      }
-    },
-    props: {
-      cancelText: {
-        type: String,
-        default: '取消'
-      },
-      confirmText: {
-        type: String,
-        default: '确定'
-      },
-        visibleItemCount: {
-            type: Number,
-            default: 7
+import { Picker } from "mint-ui";
+import { Popup } from "mint-ui";
+import info from "./address";
+let components = {};
+components[Picker.name] = Picker;
+components[Popup.name] = Popup;
+export default {
+  name: "address-picker",
+  components: components,
+  data() {
+    return {
+      slots: [
+        {
+          flex: 1,
+          values: info,
+          className: "province",
+          textAlign: "center"
         },
-      value : {
-        type : Boolean,
-        defualt : false
+        {
+          flex: 1,
+          values: info[0].city,
+          className: "city",
+          textAlign: "center"
+        },
+        {
+          flex: 1,
+          values: info[0].city[0].district,
+          className: "area",
+          textAlign: "center"
+        }
+      ],
+      selectedAddress: {},
+      visible: false
+    };
+  },
+  props: {
+    cancelText: {
+      type: String,
+      default: "取消"
+    },
+    confirmText: {
+      type: String,
+      default: "确定"
+    },
+    visibleItemCount: {
+      type: Number,
+      default: 7
+    },
+    value: {
+      type: Boolean,
+      defualt: false
+    }
+  },
+  methods: {
+    onValuesChange(picker, values) {
+      // 防止没有省份时报错
+      if (values[0]) {
+        picker.setSlotValues(1, values[0].city);
+        this.selectedAddress.province = {
+          id: values[0].id,
+          name: values[0].name
+        };
+      }
+      // 防止没有市时报错
+      if (values[1]) {
+        picker.setSlotValues(2, values[1].district);
+        this.selectedAddress.city = { id: values[1].id, name: values[1].name };
+      }
+      // 防止没有区时报错
+      if (values[2]) {
+        this.selectedAddress.district = {
+          id: values[2].id,
+          name: values[2].name
+        };
       }
     },
-    methods : {
-      onValuesChange (picker, values) {
-        // 防止没有省份时报错
-        if (values[0]) {
-          this.slots[1].values = values[0].city;
-          this.selectedAddress.province = {id : values[0].id, name : values[0].name};
-        }
-        // 防止没有市时报错
-        if (values[1]) {
-          this.slots[2].values = values[1].district
-          this.selectedAddress.city = {id : values[1].id, name : values[1].name};
-        }
-        // 防止没有区时报错
-        if (values[2]) {
-          this.selectedAddress.district = {id : values[2].id, name : values[2].name}
-        }
-      },
-      confirm () {
-        this.visible = false;
-        this.$emit('confirm', this.selectedAddress);
-      },
-      open () {
-        this.visible = true;
-      }
+    confirm() {
+      this.visible = false;
+      this.$emit("confirm", this.selectedAddress);
     },
-    watch : {
-      value (val) {
-        this.visible = val;
-      },
-      visible (val) {
-        this.$emit('input', val);
-      }
+    open() {
+      this.visible = true;
+    }
+  },
+  watch: {
+    value(val) {
+      this.visible = val;
+    },
+    visible(val) {
+      this.$emit("input", val);
     }
   }
+};
 </script>
 <style scoped>
-    .mint-datetime {
-        width: 100%;
-    }.mint-datetime .picker-slot-wrapper, .mint-datetime .picker-item {
-         -webkit-backface-visibility: hidden;
-         backface-visibility: hidden;
-     }
-    .mint-datetime .picker-toolbar {
-        border-bottom: solid 1px #eaeaea;
-    }
-    .mint-datetime-action {
-        display: inline-block;
-        width: 50%;
-        text-align: center;
-        line-height: 40px;
-        font-size: 16px;
-        color: #26a2ff;
-    }
-    .mint-datetime-cancel {
-        float: left;
-    }
-    .mint-datetime-confirm {
-        float: right;
-    }
-    .picker-item{
-        font-size: .32rem;
-    }
+.mint-datetime {
+  width: 100%;
+}
+.mint-datetime .picker-slot-wrapper,
+.mint-datetime .picker-item {
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+.mint-datetime .picker-toolbar {
+  border-bottom: solid 1px #eaeaea;
+}
+.mint-datetime-action {
+  display: inline-block;
+  width: 50%;
+  text-align: center;
+  line-height: 40px;
+  font-size: 16px;
+  color: #26a2ff;
+}
+.mint-datetime-cancel {
+  float: left;
+}
+.mint-datetime-confirm {
+  float: right;
+}
+.picker-item {
+  font-size: 0.32rem;
+}
 </style>
